b6cc13a0d74321223168ad951446ff2a86998012
Sukeshk6 <iamsukeshk@gmail.com>
2018-06-14 08:46:22 +0530

    Build version 2018.06.08

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^music/',include('music.urls')),
+    url(r'^login/$', views.login,{'template_name':'music/login.html'}, name='login'),
+    url(r'^logout/$',views.logout,name='logout')
 ]
 
 if settings.DEBUG:
